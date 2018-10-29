2d464c199740d4723f0c61941a35a9de295715f6
april <martinezapril56@yahoo.com>
2018-10-29 11:00:57 -0400

    april

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,12 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-    {
-        'author': 'MartinezMangement',
-        'title': 'Blog Post 1 ',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'MartinezMangement',
-        'title': 'Blog Post 2 ',
-        'content': 'Second post content',
-        'date_posted': 'August 30, 2018'
-    }
-]
+
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts' : Post.objects.all()
     }
 
     return render(request, 'blog/home.html', context)
