ef9bddb32c92d2387dfb8891c0a0f94884689276
april <martinezapril56@yahoo.com>
2018-10-30 15:37:55 -0400

    april

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 
 class Post(models.Model):
-    title = models.name = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
