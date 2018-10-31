d257237bab9ba52615bb4b3ceaa12e0c45dd38da
april <martinezapril56@yahoo.com>
2018-10-31 16:34:02 -0400

    april

--- users/views.py
+++ users/views.py
@@ -7,15 +7,14 @@ from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
-        if form.is_valid():             
-            username = form.cleaned_data.get('username')
+        if form.is_valid():
             form.save()
+            username = form.cleaned_data.get('username')
             messages.success(
                 request, f'Your account has been created! You are now able to log in')
             return redirect('login')
     else:
         form = UserRegisterForm()
-
     return render(request, 'users/register.html', {'form': form})
 
 
